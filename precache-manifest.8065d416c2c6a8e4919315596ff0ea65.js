74886ca9bd7d3d0410ce289a59ae8053483d042c
yanlei <yanlei@guomaixinan.com>
2021-11-18 14:00:58 +0800

    1

---
+++ precache-manifest.8065d416c2c6a8e4919315596ff0ea65.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5970c7c827645572ca",
+    "url": "/pwa/css/app.4bf94556.css"
+  },
+  {
+    "revision": "95e9b3be0b482cc23a44",
+    "url": "/pwa/css/chunk-f2d09168.94f9a8a7.css"
+  },
+  {
+    "revision": "70901171bf523d33e9db",
+    "url": "/pwa/css/chunk-vendors.0d9b0086.css"
+  },
+  {
+    "revision": "0bc9958ed5c89df9e4e55f8518f46ab8",
+    "url": "/pwa/img/empty-image-default.0bc9958e.png"
+  },
+  {
+    "revision": "75774dd6a2f0c76fd1b8ca8ba028f6e0",
+    "url": "/pwa/img/empty-image-search.75774dd6.png"
+  },
+  {
+    "revision": "28f25a952837b4421547f8cee43b762b",
+    "url": "/pwa/img/iconfont.28f25a95.svg"
+  },
+  {
+    "revision": "01404965731551593b9541784c6e6d42",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "ab5970c7c827645572ca",
+    "url": "/pwa/js/app.a0850d84.js"
+  },
+  {
+    "revision": "95e9b3be0b482cc23a44",
+    "url": "/pwa/js/chunk-f2d09168.df926224.js"
+  },
+  {
+    "revision": "70901171bf523d33e9db",
+    "url": "/pwa/js/chunk-vendors.109701c4.js"
+  },
+  {
+    "revision": "7a957f9180c332f6e23d",
+    "url": "/pwa/js/login.7ee9d2ad.js"
+  },
+  {
+    "revision": "cbde12b97b87eab0b6d01e1fcc7b80fd",
+    "url": "/pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  }
+]);
